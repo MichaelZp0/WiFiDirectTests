69f55f972bea15aa96e66cc727bd42d5f4502ca0
Michael Zappe <michaelzappe@microsoft.com>
2025-03-12 10:09:35 +0100

    Init

---
+++ Client/main.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+#include <winrt/Windows.Devices.Enumeration.h>
+#include <winrt/Windows.Devices.WiFiDirect.h>
+#include <iostream>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Devices::Enumeration;
+using namespace Windows::Devices::WiFiDirect;
+
+void OnDeviceAdded(DeviceWatcher const& watcher, DeviceInformation const& deviceInfo)
+{
+    std::wcout << L"Device found: " << deviceInfo.Name().c_str() << std::endl;
+}
+
+void OnDeviceRemoved(DeviceWatcher const& watcher, DeviceInformationUpdate const& deviceInfoUpdate)
+{
+    std::wcout << L"Device removed: " << deviceInfoUpdate.Id().c_str() << std::endl;
+}
+
+void OnDeviceUpdated(DeviceWatcher const& watcher, DeviceInformationUpdate const& deviceInfoUpdate)
+{
+    std::wcout << L"Device updated: " << deviceInfoUpdate.Id().c_str() << std::endl;
+}
+
+void OnEnumerationCompleted(DeviceWatcher const& watcher, winrt::Windows::Foundation::IInspectable const& deviceInfo)
+{
+    std::wcout << L"Device enumeration completed: " << std::endl;
+}
+
+void OnStopped(DeviceWatcher const& watcher, winrt::Windows::Foundation::IInspectable const& deviceInfo)
+{
+    std::wcout << L"DeviceWatcher stopped: " << std::endl;
+}
+
+int main()
+{
+    init_apartment();
+
+    winrt::hstring deviceSelector = WiFiDirectDevice::GetDeviceSelector(WiFiDirectDeviceSelectorType::DeviceInterface);
+    std::vector<winrt::hstring> requestProperties;
+    requestProperties.push_back(L"System.Devices.WiFiDirect.InformationElements");
+
+    DeviceWatcher deviceWatcher = DeviceInformation::CreateWatcher(deviceSelector, requestProperties);
+
+    deviceWatcher.Added(OnDeviceAdded);
+    deviceWatcher.Removed(OnDeviceRemoved);
+    deviceWatcher.Updated(OnDeviceUpdated);
+    deviceWatcher.EnumerationCompleted(OnEnumerationCompleted);
+    deviceWatcher.Stopped(OnStopped);
+
+    deviceWatcher.Start();
+
+    std::wcout << L"Searching for WiFi Direct devices..." << std::endl;
+
+    // Keep the application running to allow asynchronous operations to complete
+    std::string input;
+    std::getline(std::cin, input);
+
+    deviceWatcher.Stop();
+
+    return 0;
+}
---
+++ Server/main.cpp
@@ -0,0 +1,91 @@
+﻿#include "pch.h"
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Devices.WiFiDirect.h>
+#include <winrt/Windows.Networking.Sockets.h>
+#include <winrt/Windows.Devices.Enumeration.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <iostream>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Devices::WiFiDirect;
+using namespace Windows::Networking::Sockets;
+using namespace winrt::Windows::Storage::Streams;
+
+void OnConnectionRequested(WiFiDirectConnectionListener const &sender, WiFiDirectConnectionRequestedEventArgs const &connectionEventArgs)
+{
+    auto connectionRequest = connectionEventArgs.GetConnectionRequest();
+    std::wcout << L"Connection requested from: " << connectionRequest.DeviceInformation().Name().c_str() << std::endl;
+
+    WiFiDirectDevice wfdDevice = WiFiDirectDevice::FromIdAsync(connectionRequest.DeviceInformation().Id()).get();
+    StreamSocketListener listener;
+    listener.ConnectionReceived([](StreamSocketListener const& listener, StreamSocketListenerConnectionReceivedEventArgs const& args)
+        {
+            std::wcout << L"Connection received!" << std::endl;
+            // Handle the incoming connection
+        });
+
+    listener.BindServiceNameAsync(L"50001").get();
+    std::wcout << L"Listening for incoming connections on port 50001..." << std::endl;
+}
+
+int main()
+{
+    init_apartment();
+
+    WiFiDirectAdvertisementPublisher publisher;
+
+    //publisher.Advertisement().IsAutonomousGroupOwnerEnabled(true);
+    publisher.Advertisement().IsAutonomousGroupOwnerEnabled(false);
+
+    //publisher.Advertisement().ListenStateDiscoverability(WiFiDirect::WiFiDirectAdvertisementListenStateDiscoverability::None);
+    publisher.Advertisement().ListenStateDiscoverability(WiFiDirectAdvertisementListenStateDiscoverability::Normal);
+    //publisher.Advertisement().ListenStateDiscoverability(WiFiDirect::WiFiDirectAdvertisementListenStateDiscoverability::Intensive);
+
+    publisher.StatusChanged([](WiFiDirectAdvertisementPublisher const& publisher, WiFiDirectAdvertisementPublisherStatusChangedEventArgs const& args)
+        {
+            std::wcout << L"Advertisement status: " << static_cast<int32_t>(args.Status()) << std::endl;
+        });
+
+    // Add publisher IE
+    WiFiDirectInformationElement IE;
+
+    param::hstring IE_String = L"IE_String";
+
+    DataWriter dataWriter;
+    dataWriter.UnicodeEncoding(UnicodeEncoding::Utf8);
+    dataWriter.ByteOrder(ByteOrder::LittleEndian);
+    dataWriter.WriteUInt32(dataWriter.MeasureString(IE_String));
+    dataWriter.WriteString(IE_String);
+    IE.Value(dataWriter.DetachBuffer());
+
+    const char* customOuiStr = "CustomOui_Txt";
+    std::array<uint8_t, 13> customOuiStdArr{};
+    for (int i = 0; i < customOuiStdArr.size(); ++i)
+    {
+        customOuiStdArr[i] = static_cast<uint8_t>(customOuiStr[i]);
+    }
+
+    DataWriter dataWriterOUI;
+    dataWriterOUI.WriteBytes(customOuiStdArr);
+    IE.Oui(dataWriterOUI.DetachBuffer());
+
+    IE.OuiType(0xDD);
+
+    publisher.Advertisement().InformationElements().Append(IE);
+
+    WiFiDirectConnectionListener listener;
+    listener.ConnectionRequested(OnConnectionRequested);
+
+    publisher.Start();
+    std::wcout << L"Started WiFi Direct advertisement..." << std::endl;
+
+    // Keep the application running to allow asynchronous operations to complete
+    std::string input;
+    std::getline(std::cin, input);
+
+    publisher.Stop();
+    std::wcout << L"Stopped WiFi Direct advertisement." << std::endl;
+
+    return 0;
+}
